26835b7d46a0709b699b8dcfe3ac704223fc17ad
sebastian <sebastian.osorio.montano@gmail.com>
2025-04-14 16:58:56 +0200

    14/04/2025 subida proyecto Spring

---
+++ backend/iesports/src/main/java/example/iesports/IesportsApplication.java
@@ -0,0 +1,13 @@
+package example.iesports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IesportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IesportsApplication.class, args);
+	}
+
+}
---
+++ backend/iesports/src/test/java/example/iesports/IesportsApplicationTests.java
@@ -0,0 +1,13 @@
+package example.iesports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IesportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
