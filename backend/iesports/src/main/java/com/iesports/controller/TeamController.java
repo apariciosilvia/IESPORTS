f8d1c3933ba55c945b2bcc560c7e12566c723a0f
apariciosilvia <silvia.aparicio.martin@gmail.com>
2025-05-24 19:20:52 +0200

    Cambio endpoint de controlador

--- backend/iesports/src/main/java/com/iesports/controller/TeamController.java
+++ backend/iesports/src/main/java/com/iesports/controller/TeamController.java
@@ -9,12 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.iesports.dao.service.impl.TeamServiceImpl;
-import com.iesports.dao.service.impl.TournamentServiceImpl;
 import com.iesports.dto.TeamInfoDTO;
-import com.iesports.dto.TournamentAdminDTO;
 import com.iesports.model.Team;
-import com.iesports.model.Tournament;
 
 @RestController
 @RequestMapping("/team")
@@ -22,9 +20,6 @@ public class TeamController {
 	
 	@Autowired
 	TeamServiceImpl tr;
-	
-	@Autowired
-	TournamentServiceImpl tournamentS;
 	
 	@GetMapping("/getTeamsInfo")
 	public ResponseEntity<List<TeamInfoDTO>> getTeamsInfo(){
@@ -37,33 +32,9 @@ public class TeamController {
 			currentTeamInfo.setTeam(team);
 			currentTeamInfo.setSports(tr.getSportsByIdTeam(team.getId()));
 			
-			
 			result.add(currentTeamInfo);
 			
-			
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
-	
-	@GetMapping("/getTeamsByTournamentId")
-	public ResponseEntity<?> getTeamsByTournamentId(){
-		List<TournamentAdminDTO> result = new ArrayList<TournamentAdminDTO>();
-		
-		List<Tournament> tournaments = tournamentS.getTournaments();
-		
-		for (Tournament currentTournament : tournaments)
-		{
-			TournamentAdminDTO newTournamentAdmin = new TournamentAdminDTO();
-			List<Team> teamsCurrentTournament = tr.getTeamsByTournamentId(currentTournament.getId());
-			
-			newTournamentAdmin.setTournament(currentTournament);
-			newTournamentAdmin.setTeams(teamsCurrentTournament);
-			
-			
-			
-			result.add(newTournamentAdmin);
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(result);
-	}
-
 }
